069c7635026f3e3fc403b61ae19359e902e25449
Junior <juniorgtr1@gmail.com>
2021-10-03 11:36:25 -0300

    projeto inicializado

---
+++ src/main/java/com/devloopsapp/finappcontroller/FinappControllerApplication.java
@@ -0,0 +1,13 @@
+package com.devloopsapp.finappcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinappControllerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinappControllerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devloopsapp/finappcontroller/FinappControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devloopsapp.finappcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinappControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
